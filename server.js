71b7da3d7aeec73e399ef4fe8d2cad77b7a7144b
Carlos Macias <cimm21187@gmail.com>
2023-09-27 13:18:37 -0400

    Refactor server setup: extracted API routes, improved path consistency, and updated PORT

--- server.js
+++ server.js
@@ -1,50 +1,29 @@
 // Packages needed for this application
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
-const { v4: uuidv4 } = require("uuid");
+const { clog } = require("./middleware/clog");
+const api = require("./routes/index.js");
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+app.use(clog);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use("/api", api);
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        res.json(JSON.parse(data));
-    });
-});
-
-app.post('/api/notes', (req, res) => {
-    const newNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuidv4()
-    };
-
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        notes.push(newNote);
-
-        fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), (err) => {
-            if (err) throw err;
-            res.json(newNote);
-        });
-    });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`App listening on PORT ${PORT}`);
+  console.log(`App listening at http://localhost:${PORT}`);
 });
